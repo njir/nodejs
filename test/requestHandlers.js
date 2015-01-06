bd5cc891c4967b4037e2385cc6a2e58fc47e156a
njir <njir@naver.com>
2015-01-06 01:15:01 +0000

    node.js post

--- test/requestHandlers.js
+++ test/requestHandlers.js
@@ -1,18 +1,30 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-
-function start(response){
+function start(response, postData){
     console.log("Request handler 'start' was called");
     
-    exec("ls -lah", function(error, stdout, stderr){
-        response.write(stdout);
-        response.end();
-    });
+    var body = '<html>' + 
+    '<head>' +
+    '<meta http-equiv="Content-Type" content="text/html; ' +
+    'charset=utf-8 />' +
+    '</head>' +
+    '<body>' +
+    '<form action="/upload" method="post">' +
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submt" value="Submit text" />' +
+    '</form>' +
+    '</body>'+
+    '</html>';
+    
+    response.write(200, {"Contet-Type": "text/html"});
+    response.write(body);
+    response.end();
 }
 
-function upload(response){
+function upload(response, postData){
     console.log("Request handler 'upload' was called");
-    response.write("Hello Upload");
+    response.write(200, {"Contet-Type": "text/html"});
+    response.write("You've send : " +  querystring.parse(postData).text);
     response.end();
 }
 
--- test/server.js
+++ test/server.js
@@ -3,11 +3,24 @@ var url = require("url");
 
 function start(route, handle){
     function onRequest(request, response){
+        var postData = "";
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received");
         
-        route(handle, pathname, response);
+        request.setEncoding("utf8");
+        
+        request.addListener("data", function(postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data chunk '" +
+            postDataChunk + "'.");
+        });
+        
+        request.addListener("end", function(){
+            route(handle, pathname, response, postData);
+        });
+    
     }
+    
     http.createServer(onRequest).listen(process.env.PORT, process.env.IP);
     console.log("Server has started");
 }
@@ -16,4 +29,4 @@ exports.start = start;
 
 // 서버를 생성할 때 서버 생성 메소드의 파라미터로 함수를 넘깁니다. (onRequest)
 // 요청이 올 때마다 파라미터로 넘긴 함수가 호출됩니다.
-// 이를 callbacas
+// 이를 callback
